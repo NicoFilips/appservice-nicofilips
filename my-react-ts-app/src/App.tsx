a861e9a001d67a4be017aaf38ae402d2a81889bf
NicoFilips <git@nicofilips.de>
2024-02-09 15:42:43 +0100

    feat: add styling

--- my-react-ts-app/src/App.tsx
+++ my-react-ts-app/src/App.tsx
@@ -6,6 +6,7 @@ import CircleImage from './components/me/CircleImage'
 import TheParticles from './components/Background/TheParticles'
 import Modal from './components/Modal/Modal'
 import CvModal from './components/Modal/CV/CvModal'
+import AboutmeModal from './components/Modal/AbouteMe/AboutMeModal'
 
 function App() {
   const [showCvModal, setshowCvModal] = useState(false);
@@ -19,32 +20,32 @@ function App() {
     </div>
       <div>
         <a href="https://GitHub.com/NicoFilips" target="_blank">
-          <img src={GithubLogo} className="logo" alt="GitHub logo" />
+          <img src={GithubLogo} className="logo" alt="GitHub logo" style={{ userSelect: 'none' }} />
         </a>
         <a href="https://www.linkedin.com/in/nicofilips/" target="_blank">
-          <img src={linkedinLogo} className="logo" alt="LinkedIn logo" />
+          <img src={linkedinLogo} className="logo" alt="LinkedIn logo" style={{ userSelect: 'none' }} />
         </a>
       </div>
       <h1 className="whiteh1">Hey!</h1>
-      <h1 className="whiteh1">💫 I'm Nico Filips, A Backend Developer from Augsburg, Germany.</h1>
+      <h1 className="whiteh1">💫 I'm Nico Filips, a backend developer from Augsburg, Germany.</h1>
       <div className="card">
         <div className="cv-modal">
-        <button onClick={() => setshowCvModal(true)}>CV</button>
+        <button  className="button-hover-effect" onClick={() => setshowCvModal(true)}>💼</button>
         <Modal show={showCvModal} onClose={() => setshowCvModal(false)}>
         <CvModal/>
       </Modal>
-      <button onClick={() => setShowAboutMeModal(true)}>About me</button>
+      <button className="button-hover-effect" onClick={() => setShowAboutMeModal(true)}>🙋‍♂️</button>
         <Modal show={showAboutMeModal} onClose={() => setShowAboutMeModal(false)}>
-        <CvModal/>
+        <AboutmeModal/>
       </Modal>
-      <button onClick={() => setShowTechStackModal(true)}>Techstack</button>
+      <button className="button-hover-effect" onClick={() => setShowTechStackModal(true)}>👨‍💻</button>
         <Modal show={showTechStackModal} onClose={() => setShowTechStackModal(false)}>
         <CvModal/>
       </Modal>
         </div>
       </div>
       <TheParticles />
-      <p className="read-the-docs">
+      <p className="read-the-docs" style={{ userSelect: 'none' }} >
         This Site is hosted as an App Service on Azure.
       </p>
     </>
---
+++ my-react-ts-app/src/components/Modal/AbouteMe/AboutMeModal.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const AboutmeModal: React.FC = () => {
+  return (
+    <div>
+      <h2>About me</h2>
+      
+      <section>
+        <ul>
+        <p>As a mid-career software developer and dedicated ice hockey referee in professional leagues, I bring a wealth of experience in both the technical and athletic realms. In my professional life, I have developed a broad range of backend and frontend applications using a tech stack that includes <strong>C#</strong>, <strong>SQL</strong> and <strong>Python</strong>, developing APIs with <strong>.Net 6</strong> and building <strong>Node.Js SPAs</strong> with <strong>React</strong> and <strong>Typescript</strong>. I'm using <strong>Selenium</strong> and <strong>XUnit</strong> to test my applications, I'm also familiar with a variety of Tools in <strong>Azure</strong>. Besides that <strong>Docker</strong> and <strong>Kubernetes</strong> to host my Apps.</p>
+        <p>I am constantly striving to improve my skills and stay current in the fast-paced world of technology. As a passionate and motivated individual, I am excited to take on new challenges in the workplace and on the ice.</p>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default AboutmeModal;
